f0a2621b1e406d683f766a2be4b71cb3f699a707
loucks.dev <parkerloucks74@gmail.com>
2026-04-26 13:46:20 -0500

    Floor/contract randomizer skeleton

---
+++ scripts/RunGenerator.gd
@@ -0,0 +1,21 @@
+#  Every contract start, generate a run object 
+extends Node
+
+var run_id
+var contract_type # get from ContractResource
+var total_floors # equal contract_type.total_floors
+var floors = []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
